2385d7eb841af36912a31fff58543267812e0421
Peter Hindes <19994487+PeterHindes@users.noreply.github.com>
2025-01-29 16:35:44 -0700

    rebuilding site Wed Jan 29 16:35:44 MST 2025

--- deploy.sh
+++ deploy.sh
@@ -26,3 +26,17 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push
+
+cd ..
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push
